84b42f5cf97fb6e553c7639c2cf286ad21ce475e
julie0005 <duliesoures@gmail.com>
2020-09-08 21:44:18 +0900

    UI and DragDrop

---
+++ Assets/Scripts/DragDrop.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour
+{
+    private bool isDragging = false;
+    private Vector2 startPosition;
+    public GameObject Card;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDragging)
+        {
+            Card.transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+           
+    }
+
+    public void StartDrag()
+    {
+
+        startPosition = Card.transform.position;
+        isDragging = true;
+    }
+
+    public void EndDrag()
+    {
+        isDragging = false;
+    }
+}
